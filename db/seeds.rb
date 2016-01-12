09436d6aa3d8abfb152de59da9564638ca1bf8f5
Rachel Warbelow <rachelwarbelow@gmail.com>
2016-01-12 11:50:41 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Level.create(
+  title: "Easy", 
+  text:  "This is the easy level."
+)
+
+Level.create(
+  title: "Medium", 
+  text:  "This piece of text has words with capital letters, like Rails and Ruby."
+)
+
+Level.create(
+  title: "Difficult", 
+  text:  "Pouvez-vous taper une autre langue comme le français?"
+)
+
+Level.create(
+  title: "Inferno", 
+  text:  "K&+6L K7D~!$Sao Ffd,HJb|kh*(^0loIH)(&) LiugIHFd=of}yd0 8(&(*^9*h ilUGilug 7Iy<t97T98*)&FDY(S*Y08 y(*Y9*+H9*579nb^"
+)
