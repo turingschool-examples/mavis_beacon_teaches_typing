e5d11c9b844030346b4e11a1b88ccf1309e5e58f
Ashwin Rao <theonlyrao@gmail.com>
2016-04-23 20:42:15 -0600

    Get math working

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -1,8 +1,22 @@
+require 'bigdecimal'
+
 class Attempt < ActiveRecord::Base
   belongs_to :level
   validates :text, presence: true
 
   def percent_correct
-    byebug
+    correct = level.text.delete(" ").chars
+    # comparison = self.text.delete(" ").chars.zip(correct)
+    comparison = correct.zip(self.text.delete(" ").chars)
+    wrong_counter = 0
+    comparison.each do |ans, right|
+      wrong_counter += 1 if ans != right
+    end
+    wrong = BigDecimal(wrong_counter)
+    correct_count = BigDecimal(correct.count)
+    percent_wrong = ((wrong / correct_count).to_f) * 100
+    100 - percent_wrong.floor
+    # percent_correct = BigDecimal((1 - (wrong / correct_count)) * 100)
+    # percent_correct.floor
   end
 end
