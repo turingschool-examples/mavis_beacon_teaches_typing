2adf5c94d11ab1d031f0eac6ace59d2ce4de550f
Ashwin Rao <theonlyrao@gmail.com>
2016-04-23 20:51:33 -0600

    Refactor percent correct methods

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -5,18 +5,37 @@ class Attempt < ActiveRecord::Base
   validates :text, presence: true
 
   def percent_correct
-    correct = level.text.delete(" ").chars
-    # comparison = self.text.delete(" ").chars.zip(correct)
-    comparison = correct.zip(self.text.delete(" ").chars)
-    wrong_counter = 0
-    comparison.each do |ans, right|
-      wrong_counter += 1 if ans != right
+    compare(correct, answer)
+    calculate_percent_correct
+  end
+
+  private
+
+  def correct
+    # get rid of spaces in level_text and get array of chars
+    level.text.delete(" ").chars
+  end
+
+  def answer
+    # get rid of spaces in attempt_text and get array of chars
+    text.delete(" ").chars
+  end
+
+  def compare(correct, answer)
+    # zip the two arrays together, starting with level_text to get
+    # the math to work out right so that a attempt_text that is
+    # shorter doesn't cut off parts of the level_text
+    @wrong_counter = 0
+    correct.zip(answer).each do |ans, right|
+      @wrong_counter += 1 if ans != right
     end
-    wrong = BigDecimal(wrong_counter)
+  end
+
+  def calculate_percent_correct
+    # good luck with the arithmatic
+    wrong = BigDecimal(@wrong_counter)
     correct_count = BigDecimal(correct.count)
     percent_wrong = ((wrong / correct_count).to_f) * 100
     100 - percent_wrong.floor
-    # percent_correct = BigDecimal((1 - (wrong / correct_count)) * 100)
-    # percent_correct.floor
   end
 end
